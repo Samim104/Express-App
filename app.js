4319a8baa6603f1c80a90296a6577a90a6fed7e8
Flash <samimdillinger121@gmail.com>
2022-02-01 23:04:20 +0530

    modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.get('/', async (req, res) => {
     const jsondata = await db.find()
-    res.send(jsondata[3])
+    res.send(jsondata)
 })
 
 app.listen(port, () => { console.log("> app listening on port " + port); })
